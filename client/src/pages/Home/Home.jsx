772eee12df4f28af1f743aa292dd48ec1ae7cf96
Nate <natewillimack@gmail.com>
2024-08-28 00:18:43 -0400

    edited home page suggestions

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -8,11 +8,12 @@ const Home = () => {
       <div className="parallax">
         <div className="parallax-content">
           <h1>Welcome to Our Website</h1>
-          <p>Experience the dynamic feel with parallax scrolling.</p>
+          <p>Jake we need to finish this page. See if you can convice Joe to design a picture... otherwise we could probably just
+            use ai to create one. I can fill out the description later.</p>
         </div>
       </div>
       <div className="container">
-        <h2>About Us</h2>
+        <h2>About Us: This needs filled out too. Do you like the layout to this or should we revamp the entire home page?</h2>
         <p>
           We rock.
         </p>
