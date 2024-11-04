eb2b0100dadca77bbef793c325a4f9a5306a8c7b
Jakethetyper8 <jacobjennings7991@gmai.com>
2024-11-03 21:51:21 -0600

    updates

--- client/src/pages/Profile/Profile.jsx
+++ client/src/pages/Profile/Profile.jsx
@@ -25,7 +25,7 @@ const Profile = ({ userEmail, userFinancials, setUserFinancials }) => {
           </select>
         </div>
         <div>
-          <label>Outlook:</label>
+          <label>Period:</label>
           <select>
             {outlookOptions.map((option, index) => (
               <option value={option.value} key={index}>
@@ -41,26 +41,72 @@ const Profile = ({ userEmail, userFinancials, setUserFinancials }) => {
         </div>
         <div className="Box">
           <h2>Incomes</h2>
-          <ul>
-            <li className="financialHeader">
-              <div>
-                <strong>Income Source:</strong>
+          {userFinancials.incomes.length > 0 ? (
+            <>
+              <div className="financialHeader">
+                <div>
+                  <strong>Income Source:</strong>
+                </div>
+                <div>Amount</div>
               </div>
-              <div>Amount</div>
-            </li>
-            {userFinancials.incomes.map((income, index) => (
-              <li key={index} className="financialBlock">
-                <div>{income.name}</div>
-                <div>${income.amount}</div>
-              </li>
-            ))}
-          </ul>
+              <ul className="financialsBlock">
+                {userFinancials.incomes.map((income, index) => (
+                  <li key={index} className="financialBlock">
+                    <div>{income.name}</div>
+                    <div>${income.amount}</div>
+                  </li>
+                ))}
+              </ul>
+            </>
+          ) : (
+            <div>No Investments Recorded</div>
+          )}
         </div>
         <div className="Box">
           <h2>Expenses</h2>
+          {userFinancials.expenses.length > 0 ? (
+            <>
+              <div className="financialHeader">
+                <div>
+                  <strong>Expense:</strong>
+                </div>
+                <div>Amount</div>
+              </div>
+              <ul>
+                {userFinancials.expenses.map((expense, index) => (
+                  <li key={index} className="financialBlock">
+                    <div>{expense.name}</div>
+                    <div>${expense.amount}</div>
+                  </li>
+                ))}
+              </ul>
+            </>
+          ) : (
+            <div>No Investments Recorded</div>
+          )}
         </div>
         <div className="Box">
           <h2>Investments</h2>
+          {userFinancials.investments.length > 0 ? (
+            <>
+              <div className="financialHeader">
+                <div>
+                  <strong>Investment:</strong>
+                </div>
+                <div>Amount</div>
+              </div>
+              <ul>
+                {userFinancials.investments.map((investment, index) => (
+                  <li key={index} className="financialBlock">
+                    <div>{investment.name}</div>
+                    <div>${investment.amount}</div>
+                  </li>
+                ))}
+              </ul>
+            </>
+          ) : (
+            <div>No Investments Recorded</div>
+          )}
         </div>
       </div>
     </div>
